610df443f7702472041452d4b53a946f2567a2f2
SamikshyaKar <Write2samikshyakar@gmail.com>
2023-05-23 17:12:29 +0530

    react element

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-    <h1>Online Shopping World</h1>
-    </div>
+    // <div className="App">
+    // <h1>Online Shopping World</h1>
+    // </div>
+    React.createElement('div',null,React.createElement('h1',null,'Online shopping World'))
   );
 }
 
